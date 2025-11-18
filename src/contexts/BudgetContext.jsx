0d8c0add5ac2d057cdcf1dc0544a9fe5c7cc9374
FrancescoTroise <francesco.troise98@gmail.com>
2025-11-18 16:01:53 +0100

    Fix: Initialize the BudgetContext to "false"

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -2,7 +2,7 @@
 import { createContext } from "react";
 
 //Definizione del Context
-const BudgetContext = createContext();
+const BudgetContext = createContext(false);
 
 //export del Context
 export default BudgetContext;
