e350473be84752f51b5d78b0550b8671881ab557
FrancescoTroise <francesco.troise98@gmail.com>
2025-11-18 15:48:23 +0100

    MILESTONE 1
    Add BudgetContext definition (previous commit)
    
    Add state (budgetMode)
    
    Insert state and its setter into BudgetContext
    
    Wrap App.jsx using the BudgetContext .Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+//Import del Context definito in BudgetContext.jsx
+import { BudgetContext } from "./contexts/BudgetContext";
+
+//Import dello stato budgetMode
+import { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/Homepage";
@@ -6,19 +12,27 @@ import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
 
 function App() {
+  const [budgetMode, setBudgetMode] = useState(true);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<HomePage />}></Route>
-            <Route path="/AboutUS" element={<AboutUs />} />
-            <Route path="/Products" element={<Products />} />
-            <Route path="/products/:id" element={<ProductDetail />} />
-            {/*Percorso dinamico*/}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BudgetContext.Provider
+        value={{
+          budgetMode,
+          setBudgetMode,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<HomePage />}></Route>
+              <Route path="/AboutUS" element={<AboutUs />} />
+              <Route path="/Products" element={<Products />} />
+              <Route path="/products/:id" element={<ProductDetail />} />
+              {/*Percorso dinamico*/}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext.Provider>
     </>
   );
 }
